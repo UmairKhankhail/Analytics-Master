d3ef5cd9340f19572e7495fa06b2da74586bd497
Umair Khan <umair.khan@letsdreambeyond.com>
2024-07-30 11:44:54 +0500

    index method added.

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 app.register_blueprint(upload_bp, url_prefix='/upload')
 app.register_blueprint(query_bp, url_prefix='/query')
 
+@app.route('/')
+def index():
+    return "The application is deployed and it is running."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)

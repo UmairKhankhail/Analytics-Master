e4e9604c94c8ee6ed9540b27ab461f7ab3f7f446
Umair Khan <umair.khan@letsdreambeyond.com>
2024-07-31 18:19:46 +0500

    simple python app.

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import google.generativeai as genai
-from pandasai.llm import GoogleGemini
+#from pandasai.llm import GoogleGemini
+from pandasai.llm import GooglePalm
 
 # Set your Google API key
 GOOGLE_API_KEY = "AIzaSyBjCrtVUiixLho8kdBKY1z8kpIMn7QRcmg"
 genai.configure(api_key=GOOGLE_API_KEY)
 
-llm = GoogleGemini(api_key=GOOGLE_API_KEY, temperature=0, seed=26)
+llm = GooglePalm(api_key=GOOGLE_API_KEY, temperature=0, seed=26)

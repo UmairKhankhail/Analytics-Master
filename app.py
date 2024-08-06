d553508d5a8f4ab8c97dc6c55008a050311f984b
Umair Khan <umair.khan@letsdreambeyond.com>
2024-08-06 12:33:59 +0500

    complex methods ignored.

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from routes.upload import upload_bp
-from routes.query import query_bp
-from flask_cors import CORS
+#from routes.upload import upload_bp
+#from routes.query import query_bp
+#from flask_cors import CORS
 
 app = Flask(__name__)
 # CORS(app)
 
-app.register_blueprint(upload_bp, url_prefix='/upload')
-app.register_blueprint(query_bp, url_prefix='/query')
+#app.register_blueprint(upload_bp, url_prefix='/upload')
+#app.register_blueprint(query_bp, url_prefix='/query')
 # # Set strict_slashes to False for all routes
-app.url_map.strict_slashes = False
+#app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
